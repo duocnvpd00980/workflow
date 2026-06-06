2011c1f853861732435a0a6abf1cf957b161240a
duocnvpd00980 <lytuongdn@gmail.com>
2026-06-06 14:49:33 +0700

    Update config.py

--- api/app/marketing/config.py
+++ api/app/marketing/config.py
@@ -4,7 +4,7 @@ from pydantic import ConfigDict  # 1. Import thêm ConfigDict
 class Settings(BaseSettings):
     model_config = ConfigDict(extra="ignore", env_file=".env") 
 
-    GROQ_API_KEY: str = "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"
+    GROQ_API_KEY: str = "YOUR_GROQ_API_KEY"
     GROQ_MODEL: str = "meta-llama/llama-4-scout-17b-16e-instruct"
     DATABASE_URL: str = "sqlite:///./workflow.db"
     
