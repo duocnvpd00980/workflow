624124fb5c99ba30135e66b1b8f5b0f096c19b8b
duocnvpd00980 <lytuongdn@gmail.com>
2026-06-23 15:31:32 +0700

    ádsad

--- api/app/config.py
+++
@@ -1,8 +0,0 @@
-from functools import lru_cache
-from pydantic_settings import BaseSettings, SettingsConfigDict
-
-class Settings(BaseSettings):
-  
-@lru_cache
-def get_settings() -> Settings:
-    return Settings()
